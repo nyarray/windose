8a3873c2faaff0b3d9d10191eb84b8064bdbdf87
nyarray <88788556+nyarray@users.noreply.github.com>
2025-07-17 17:48:17 +0800

    update static files

--- config.js
+++ config.js
@@ -2,16 +2,15 @@
 const musicApi = "https://netease-cloud-music-api-x8ik.vercel.app";
 const musicListId = "7490559834";
 export default {
-    
     nitro: {
-         publicAssets: [
-    { baseURL: '/static', dir: 'public/static' }
-  ],
-//   routeRules: {
-//     '/fonts/**': {
-//       headers: { 'Content-Type': 'font/woff2' }
-//     }
-//   }
+        publicAssets: [
+            { baseURL: "/static", dir: "public/static" },
+        ],
+        //   routeRules: {
+        //     '/fonts/**': {
+        //       headers: { 'Content-Type': 'font/woff2' }
+        //     }    
+        //   }
         routeRules: {
             "/static/fonts/**.woff2": {
                 headers: {
